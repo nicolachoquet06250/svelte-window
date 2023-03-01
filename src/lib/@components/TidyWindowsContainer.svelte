7493ab1881eab117d0410609cf0fea22cb9557a5
Nicolas Choquet <nchoquet@norsys.fr>
2023-03-01 15:27:28 +0100

    Fix du bug des tuiles rangées

--- src/lib/@components/TidyWindowsContainer.svelte
+++ src/lib/@components/TidyWindowsContainer.svelte
@@ -1,19 +1,16 @@
-{#each Object.entries($list) as [tidyWindowId, { component }]}
+{#each Object.entries($list) as [tidyWindowId, { component }] (tidyWindowId)}
   <svelte:component 
         this={component} 
         id={tidyWindowId} 
         on:upset={handleUpset(tidyWindowId)} />
 {/each}
 
-<!-- 
-  @TODO Régler le bug qui fait que quand on range les 2 fenetres et qu'on re agrandi la premiere, 
-  la case reste avec le meme logo et le meme titre 
--->
-
 <script lang="ts">
     import { useTidyWindows } from "../@composables";
 
     const { list, upset } = useTidyWindows();
+
+    list.subscribe(v => console.log(v));
 
     const handleUpset = (id: string) => () => upset(id);
 </script>
